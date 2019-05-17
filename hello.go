a8e9eb03dc6d5a18fbc891f50c999957c70f2b0e
Micke Mac <mickemac@hotmail.com>
2019-05-17 11:55:59 +0200

    b

--- hello.go
+++ hello.go
@@ -1,9 +1,23 @@
 package uno
 
-import "fmt" 
+import "fmt"
 
 // Hi returns a friendly greeting
 func Hi(name string) string {
-   return fmt.Sprintf("Hi, %s !!", name)
+	return fmt.Sprintf("Hi, %s !!", name)
 }
 
+type Counter struct {
+	Value int
+}
+
+func (c *Counter) Inc()    { c.Value++ }
+func NewCounter() *Counter { return &Counter{5} }
+
+type Printer interface {
+	Print(s string) string
+}
+
+func PrintHello(p Printer) string {
+	return p.Print("Hello, World!")
+}
